d763b932f4a3c1c8bfc05a589b9b11412ef1e71a
Vauff <mctehkitti@gmail.com>
2016-04-17 12:52:48 -0400

    Added MySQL connection and overall quote viewing

--- index.php
+++ index.php
@@ -1,3 +1,7 @@
+<?php 
+require('api.php');
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
@@ -7,17 +11,17 @@
         		font-family: Unicorn;
         		src: url('fonts/unicorn.ttf');
         	}
-        
+        	
             body
             {
                 padding-top: 20px;
             }
-           
+            
             html body
             {
                 background-color: #98AFC7;
             }
-           
+            
             h1
             {
                 text-align: center;
@@ -27,6 +31,21 @@
             h3
             {
             	text-align: center;
+            }
+            
+            .quote > h4
+            {
+            	font-size: 30px !important;
+            }
+            
+            .quote > h5
+            {
+            	font-size: 24px !important;
+            }
+            
+            .quote > h6
+            {
+            	font-size: 16px !important;
             }
             
             .cstm-form-control
@@ -59,12 +78,34 @@
 	            <h1><font face="Unicorn">IRC Quotes</font></h1>
 	            <h3>By Vauff</h3>
 	            <br>
-            	<button class="btn btn-success pull-right">Add Quote</button>
-            	<form action="someAction.php">
+            	<button class="btn btn-success pull-right"><span class="glyphicon glyphicon-plus"></span> Add Quote</button>
+            	<form action="search.php">
 	            	<input class="cstm-form-control" name="quotesearch" type="search" placeholder="Search for a Quote">
-	            	<button class="btn btn-primary btn-sm" type="submit">Search</button>
+	            	<button class="btn btn-primary btn-sm" type="submit"><span class="glyphicon glyphicon-search"></span>  Search</button>
 	            </form>
                 <hr>
+                <?php
+                	$mysql = getMysql();
+                	$total = $mysql->query("SELECT COUNT(id) AS id FROM quotes")->fetch_assoc()['id'];
+                	$quotes = $mysql->query("SELECT * FROM quotes WHERE id>=".($total - 9)." AND id<=".$total." ORDER BY id DESC");
+                	
+                	while($quote = $quotes->fetch_assoc())
+                	{
+                		$id = $quote['id'];
+                		$time = $quote['time'];
+                		$ordinal = getOrdinal(date('j', $time));?>
+                		<div class="quote">
+                			<h4><a href="viewquote.php?id=<?php echo $id;?>"><b>#<?php echo $id;?> - <?php echo $quote['title'];?></b></a></h4>
+                			<h5><b>Submitter:</b> <?php echo $quote['submitter']?> - <b>Date:</b> <?php echo gmdate('l F jS, Y, g:i A T', $time);?></h5>
+                			<h6><?php echo nl2br(formatQuote($quote['quote'], array(
+                					array('<', '&lt;'),
+                					array('>', '&gt;'),
+                					array('°', '&deg;'),
+                					array('³', '&sup3;')
+                			)));?></h6>
+                		</div>
+                		<br>
+                <?php }?>
             </div>
         </div>
     </body>
